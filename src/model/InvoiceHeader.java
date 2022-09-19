7c42d60616f593741432f0b920f60eee795f7552
mohamedsaber97 <msaber9765@gmail.com>
2022-09-19 23:15:33 +0200

    first commit -- create classes and define variables

---
+++ src/model/InvoiceHeader.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Date;
+
+public class InvoiceHeader {
+
+    //definition of invoice header data
+    private int invoiceNum;
+    private Date invoiceDate;
+    private String customerName;
+}
---
+++ src/model/InvoiceLine.java
@@ -0,0 +1,10 @@
+package model;
+
+public class InvoiceLine {
+
+    //definition of invoice lines data
+    private int invoiceNum;
+    private String itemName;
+    private double itemPrice;
+    private int count;
+}
